0d0eb0a1ecb6d340e95c33804d293ed79a923a79
Amol <ajamnik.mech@gmail.com>
2023-01-20 21:19:55 +0530

    [Amol]- permutations of a String using iterative method and Recursion method

---
+++ AlgorithmPrograms/AlgorithmPrograms/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Algorithm Programs");
+            Console.WriteLine("1.Permutation ");
+            int Choice = Convert.ToInt32(Console.ReadLine());
+            switch (Choice)
+            {
+                case 1:
+                    Permutation permutation = new Permutation();
+                    String str = "ABC";
+                    int n = str.Length;
+                    permutation.permute(str, 0, n - 1);
+                    break;
+            }
+        }
+    }
+}
