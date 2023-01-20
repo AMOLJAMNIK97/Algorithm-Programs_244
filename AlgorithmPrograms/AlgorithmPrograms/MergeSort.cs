f58d977b8487e34f9ff733e4e67aee164206478b
Amol <ajamnik.mech@gmail.com>
2023-01-21 00:13:10 +0530

    [Amol]- Merge Sort

---
+++ AlgorithmPrograms/AlgorithmPrograms/MergeSort.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    public class MergeSort
+    {
+        public void mergeTwoHalf(int[] B, int z)
+        {
+            int half_i = 0;
+            // starting index of second half
+            int i;
+
+            // Temp Array store sorted resultant array
+            int[] temp = new int[z];
+
+
+            // First Find the point where array is divide
+            // into two half
+            for (i = 0; i < z - 1; i++)
+            {
+                if (B[i] > B[i + 1])
+                {
+                    half_i = i + 1;
+                    break;
+                }
+            }
+
+            // If Given array is all-ready sorted
+            if (half_i == 0)
+                return;
+
+            // Merge two sorted arrays in single sorted
+            // array
+            i = 0; int j = half_i; int k = 0;
+            while (i < half_i && j < z)
+            {
+                if (B[i] < B[j])
+                {
+                    temp[k++] = B[i++];
+                }
+                else
+                {
+                    temp[k++] = B[j++];
+                }
+            }
+            // Copy the remaining elements of A[i to half_!]
+            while (i < half_i)
+            {
+                temp[k++] = B[i++];
+            }
+            // Copy the remaining elements of A[half_!
+            // to n]
+            while (j < z) 
+            { 
+                temp[k++] = B[j++];
+            }
+
+            for (i = 0; i < z; i++) 
+                B[i] = temp[i];
+        }
+    }
+}
--- AlgorithmPrograms/AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/AlgorithmPrograms/Program.cs
@@ -11,7 +11,7 @@ namespace AlgorithmPrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Algorithm Programs");
-            Console.WriteLine("1.Permutation\n2.Binary Search Word\n3.Insertion Sort word\n4.Bubble Sort ");
+            Console.WriteLine("1.Permutation\n2.Binary Search Word\n3.Insertion Sort word\n4.Bubble Sort\n5.Merge Sort ");
             int Choice = Convert.ToInt32(Console.ReadLine());
             switch (Choice)
             {
@@ -49,6 +49,16 @@ namespace AlgorithmPrograms
                     bubble.bubbleSort(arr1, m);
                     Console.WriteLine("Sorted array");
                     bubble.printArray(arr1, m);
+                    break;
+                case 5:
+                    MergeSort mergeSort = new MergeSort();
+                    int[] B = { 2, 3, 8, -1, 7, 10 };
+                    int z = B.Length;
+                    mergeSort.mergeTwoHalf(B, z);
+                    for (int i = 0; i < z; i++)
+                    {
+                        Console.Write(B[i] + " ");
+                    } 
                     break;
             }
         }
